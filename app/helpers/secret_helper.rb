52eafa19df31e4f69d8b06f8ab0faafa60995c93
Henry Schaumburger <per4mnce@gmail.com>
2015-10-19 13:48:02 +0000

    Updated wice-Grid

--- app/helpers/secret_helper.rb
+++ app/helpers/secret_helper.rb
@@ -1,2 +1,8 @@
 module SecretHelper
+  def formatField(maxSize, fvalue)
+    if fvalue
+      #Only show the first max_size characters
+      fvalue.length > maxSize ? fvalue[0, maxSize] + "..." : fvalue
+    end
+  end
 end
