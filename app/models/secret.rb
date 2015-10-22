9539cfdec7d463feb08509b596851e5d4610dff8
Henry Schaumburger <per4mnce@gmail.com>
2015-10-22 20:06:56 +0000

    Fixed user pin

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -1,7 +1,8 @@
 class Secret < ActiveRecord::Base
+  
   belongs_to :user
   
-  userPin = "1234" #Thread.current[:session][:pin] #From login screen.  Must not be saved to DB "1234"
+  #userPin = Thread.current[:session][:pin] #From login screen.  Must not be saved to DB "1234"
   
   #Encrypt fields with the help of attr_encrypted gem 
   #See https://github.com/attr-encrypted/attr_encrypted for details
@@ -9,7 +10,7 @@ class Secret < ActiveRecord::Base
                  :pw,
                  :url,
                  :notes,
-                 :key => ENV["encryption_key"] + userPin
-                 
- puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> #{userPin} from Secret Model"
+                 :key => proc { ENV["encryption_key"] + Thread.current[:session][:pin] }
+
+  #proc forces the Thread.current[:session][:pin] to be evaluated each time
 end
