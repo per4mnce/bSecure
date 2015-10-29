e39c01315472001bf2fabfabc9779e79523d5d3f
Henry Schaumburger <per4mnce@gmail.com>
2015-10-29 18:06:55 +0000

    Updated User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,10 @@ class User < ActiveRecord::Base
   
   attr_accessor :pin, :pin_confirmation
   
-  validates :pin, length: { minimum: 8 }
+  validates :pin, length: { minimum: 8 }, on: :create
   validates :pin, confirmation: true #Make sure pin and pin_confirmation match
-  validates :pin_confirmation, presence: true
-  validate :pin_cannot_equal_password
+  validates :pin_confirmation, presence: true, on: :create
+  validate :pin_cannot_equal_password, on: :create
   
   def pin_cannot_equal_password
     #Make sure pin is not the same as the password
