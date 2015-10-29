ea80dd78a2106bc5e042420c79d664ff2b26a599
Henry Schaumburger <per4mnce@gmail.com>
2015-10-29 17:40:01 +0000

    Updated Help

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
     extensions = {
       autolink:           true,
       superscript:        true,
-      disable_indented_code_blocks: true
+      disable_indented_code_blocks: true,
     }
     
     renderer    = Redcarpet::Render::HTML.new(options)
